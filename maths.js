bfc60894e3210d949c539c64aadcf9c25328b0ed
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2024-05-09 11:15:30 -0500

    Basic binary operations

---
+++ maths.js
@@ -0,0 +1,11 @@
+//Algorithm to translate a number into binary
+
+function decimalToBinary(decimalNumber) { 
+	return decimalNumber.toString(2); 
+} 
+
+const decimalNum = 25; 
+const Result = decimalToBinary(decimalNum); 
+console.log(Result);
+
+
